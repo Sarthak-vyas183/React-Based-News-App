9082d2145030ec4f9b86b4d753ed696d1c732ad6
Sarthak vyas <142726436+Sarthak-vyas183@users.noreply.github.com>
2024-03-14 22:29:18 +0530

    news category feature builded

--- src/component/navbar.js
+++ src/component/navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link  } from "react-router-dom";
 
 export class navbar extends Component {
   render() {
@@ -6,25 +7,20 @@ export class navbar extends Component {
       <div>
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/">Navbar</a>
+    <Link className="navbar-brand" to="/">Navbar</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/">About</a>
-        </li>
-       <li><a class="nav-link" href="/">business</a></li>
-       <li><a class="nav-link" href="/">intertainment</a></li>
-       <li> <a class="nav-link" href="/">general</a></li>
-       <li> <a class="nav-link" href="/">health</a></li>
-       <li><a class="nav-link" href="/">science</a></li>
-       <li> <a class="nav-link" href="/">sports</a></li>
-       <li> <a class="nav-link" href="/">technology</a></li>
+       
+       <li> <Link className="nav-link" to="/business">Business</Link></li>
+       <li> <Link className="nav-link" to="/entertainment">Entertainment</Link></li>
+       <li> <Link className="nav-link" to="/general">General</Link></li>
+       <li> <Link className="nav-link" to="/health">Health</Link></li>
+       <li> <Link className="nav-link" to="/science">Science</Link></li>
+       <li> <Link className="nav-link" to="/sports">Sports</Link></li>
+       <li> <Link className="nav-link" to="/technology">Technology</Link></li>
       
       </ul>
      
